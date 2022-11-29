2627bae421eb4dae52e12cc439563f952386fdfe
Manoj Ravikumar Nair <Manoj.Nair@outlook.com>
2022-11-29 22:38:55 +1100

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsFirstApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public string Message { get; set; }
+    private readonly IConfiguration _configuration;
+
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public void OnGet()
+    {
+        Message = _configuration["Message"];
+    }
+}
